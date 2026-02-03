7b53b50089e733c9f8d6f4422f9a84b9dc6e599d
Ekjot Singh <ekjotmakhija@gmail.com>
2026-02-03 21:47:56 +0530

    Add mainwindow.h

---
+++ src/mainwindow.h
@@ -0,0 +1,38 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QVBoxLayout>
+#include <QLabel>
+#include "cpu8086.h"
+#include "managers/ui_manager.h"
+#include "managers/execution_manager.h"
+#include "managers/hardware_controller.h"
+
+class MainWindow : public QMainWindow {
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+
+private slots:
+    void applySettings(); // Delegate from Settings Tool
+
+private:
+    // Core
+    CPU8086 cpu;
+
+    // Components (Owned by MainWindow)
+    CodeEditor *editor;
+    RegisterView *regView;
+    MemoryView *memView;
+    QLabel *statusLabel;
+    QVBoxLayout *rightLayout;
+
+    // Managers
+    UIManager *uiManager;
+    ExecutionManager *execManager;
+    HardwareController *hwController;
+};
+
+#endif // MAINWINDOW_H
